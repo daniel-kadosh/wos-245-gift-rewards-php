48bccdc19b468160d70cd5fb558c90971391b025
Daniel Kadosh <daniel.kadosh@ieee.org>
2024-06-23 11:34:08 -0400

    docker start + rebuild script tweaks

--- wos-rebuid.sh
+++ wos-rebuid.sh
@@ -3,5 +3,4 @@ set -x
 cd /home/divergent/wos-245-gift-rewards-php
 composer update
 cp -f .env.production .env
-# --force-recreate
-sudo docker compose up --detach
+sudo docker compose build --no-cache
