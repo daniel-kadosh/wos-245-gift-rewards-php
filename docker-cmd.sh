1b538eca88e93d7e44a5402a529752552d616421
Daniel Kadosh <daniel.kadosh@ieee.org>
2024-11-10 07:45:14 -0500

    Bugfix Apache SSL configs

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # This is the "entrypoint" script run on initial container startup.
 set -x
-# Re-inject any updated Apache configs
-sudo cp /var/www/docker/00*.conf /etc/apache2/sites-available/
 
-# Run Leaf's DB migration scripts on all DBs and on success launch Apache
-# Source in application's .env for ALLIANCES list and config
+# Source in application's .env for ALLIANCES list and other config
 cd /var/www
 export $(grep -v '^#' .env | xargs)
+
+# Put together all apache configs into 1 file, re-injecting into container
+#if [[ "${APP_ENV}" = "prod" ]] ; then
+    sudo cat /var/www/docker/00*.conf > /etc/apache2/sites-available/000-default.conf
+#else
+#    sudo cp /var/www/docker/000-default.conf /etc/apache2/sites-available/000-default.conf
+#fi
+
+# Run Leaf's DB migration scripts on all DBs and on success launch Apache
 IFS=','
 read -ra array <<< "$ALLIANCES"
 echo "Setting up data dirs + DBs for $ALLIANCES"
@@ -17,9 +23,8 @@ for alliance in "${array[@]}"; do
     export DB_DATABASE=${DIR}/gift-rewards.db
     touch ${DB_DATABASE}
     # Seems all files need full permissions for web app :-(
-    #find ${DIR} -type d -exec chmod 0755 {} \;
-    chmod 666 ${DIR}/*
     find ${DIR} -type d -exec chmod 0777 {} \;
+    find ${DIR} -type f -exec chmod 0666 {} \;
     php leaf db:migrate -vvv
 done && \
 apache2-foreground
